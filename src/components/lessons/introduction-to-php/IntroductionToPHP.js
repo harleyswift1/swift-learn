2a5fd8b765bb0741d7cd9c646141082175631aba
Harley Swift <=>
2022-11-23 20:05:46 +0000

    Multiple required changes and updates

--- src/components/lessons/introduction-to-php/IntroductionToPHP.js
+++ src/components/lessons/introduction-to-php/IntroductionToPHP.js
@@ -29,7 +29,7 @@ export default function IntroductionToPHP() {
             <p>Just like other programming languages, PHP has a strict(ish) syntax that you must follow when
                 writing it's code. Most programming IDEs such as WebStorm, VSCode or Sublime Text can be used to write PHP code.</p>
             <p>You can write PHP by creating a file ending in .php, or opening PHP tags inside a PHP file.</p>
-            <PHPCode description="This is how you write Hello World in PHP" content={"<?php" + "\n  echo 'Hello World!';" + "\n" + "?>"}/>
+            <PHPCode description="This is how you write Hello World in PHP" content={"<?php \n  echo 'Hello World!'; \n ?>"}/>
             <h3>Congratulations!</h3>
             <p>You have completed your first PHP lesson! There's plenty more, so click the "Next Page" button to go to the next lesson.</p>
             <br/>
