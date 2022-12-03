980eeeb0c1de6186967aabc5b05989059f998e11
Harley Swift <=>
2022-12-03 22:04:37 +0000

    Bug and CSS fixes

--- src/components/page-lesson/side-widgets/contents/Contents.js
+++ src/components/page-lesson/side-widgets/contents/Contents.js
@@ -2,19 +2,12 @@ import "./Contents.css"
 
 export default function Contents(props) {
     const items = props.items;
-    const hrefs = props.hrefs;
-
-    function handleClick(item) {
-        document.getElementById(item).scrollIntoView({behavior: 'smooth'});
-    }
-
-
     return (<div className="contents-widget">
         <div className={"contents-widget-header"}>Table of contents</div>
 
         <div className="contents-widget-items">
             {items.map((item, index) => {
-                return (<a tabIndex={0} onClick={() => handleClick(hrefs[index])}>{index + 1}. {item}</a>)
+                return (<a tabIndex={0} href={"#" + item}>{index + 1}. {item}</a>)
             })}
         </div>
     </div>)
