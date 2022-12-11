3476ae5306781373ce24ab1e4943cb3a61992b04
Harley Swift <=>
2022-12-11 23:34:35 +0000

    Finished syntax page

--- src/components/utils/lessons/syntax/LessonSyntax.js
+++ src/components/utils/lessons/syntax/LessonSyntax.js
@@ -3,6 +3,7 @@ import Contents from "../../../page-lesson/side-widgets/contents/Contents";
 import AdvertWidget from "../../../page-lesson/side-widgets/advert/AdvertWidget";
 import PageButtons from "../../reusable/PageButtons/PageButtons";
 import Tooltip from "../../widgets/tooltip/Tooltip";
+import PHPCode from "../../widgets/php/PHPCode";
 
 export default function LessonSyntax() {
     const previousPage = "/lesson/how-it-works";
@@ -17,17 +18,30 @@ export default function LessonSyntax() {
                 It specifies how the various elements of a PHP program, such as variables, functions, and control
                 structures, should be represented and organized. </p>
             <p>The PHP syntax is based on the C programming language, so
-            it uses many of the same conventions, such as using curly braces to delimit blocks of code
-            and using the semi-colon as a statement terminator.</p>
+                it uses many of the same conventions, such as using curly braces to delimit blocks of code
+                and using the semi-colon as a statement terminator.</p>
             <Tooltip type={"tip"}
                      icon={"fa-regular fa-lightbulb"}
                      title={"Tip"}
                      content={"In general, the PHP syntax is designed to be easy to read and understand, making it a good language for beginners to learn."}/>
+            <h3 id={"Important syntax rules"}>Important syntax rules</h3>
+            <p>Some of the most important and common syntax rules for PHP are the following:</p>
+            <ol>
+                <li>Statements must end in a semi-colon ";"</li>
+                <li>Curly braces specify start and end blocks of code</li>
+                <li>Variable and function names are case sensitive</li>
+                <li>Variable and function names must not contain special characters or spaces</li>
+            </ol>
+            <PHPCode description="Here are some examples of valid and invalid PHP code" content={"$var foo = 5;              //valid \n" + "$var my variable = 5;      //invalid \n" + "$var number = 5            //invalid \n" + "$var v@r!abl3 = 5;         //invalid \n"}/>
+            <p>After a while, you will soon become super familiar with the PHP syntax, and it will
+                become second nature. In the meantime, make sure you're adding your semi-colons
+                on the end of your statements to save you 3 hours of debugging time!</p>
+
 
             <PageButtons nextPage={nextPage} previousPage={previousPage}/>
         </div>
         <div className="widgets-container">
-            <Contents items={["How is PHP run?", "When writing PHP code", "Congratulations!"]}/>
+            <Contents items={["What is the PHP syntax?", "Important syntax rules"]}/>
             <AdvertWidget/>
         </div>
     </div>)
